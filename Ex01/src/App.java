406c7fc2fb6c20a1ca9fa79f06ccab9ebb2226f4
MarlonVolff <marlonvolffsilva@gmail.com>
2024-05-10 20:49:33 -0300

    Add - App.java

--- Ex01/src/App.java
+++ Ex01/src/App.java
@@ -1,5 +1,13 @@
+import br.edu.up.models.Livro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("a");
+
+        String[] autores1 = {"Cay S. Horstmann", "Gary Cornell"};
+        Livro livro1 = new Livro("1598FHK", "Core Java 2", autores1, "0130819336", 2005);
+
+        String[] autores2 = {"Harvey Deitel"};
+        Livro livro2 = new Livro("9865PLO", "Java, Como programar", autores2, "0130341517", 2001);
+            
     }
 }
