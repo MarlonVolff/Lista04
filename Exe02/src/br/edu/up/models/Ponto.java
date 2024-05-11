bd4b89018fb5a6c83795e95cd8aecd516e3b5c7f
MarlonVolff <marlonvolffsilva@gmail.com>
2024-05-10 21:30:00 -0300

    exercicio 2

---
+++ Exe02/src/br/edu/up/models/Ponto.java
@@ -0,0 +1,35 @@
+package br.edu.up.models;
+
+public class Ponto {
+    private double x;
+    private double y;
+
+    public Ponto(Double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Ponto() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Double getX() {
+        return x;
+    }
+    
+    public void setX(Double x) {
+        this.x = x;
+    }
+    public Double getY() {
+        return y;
+    }
+    public void setY(Double y) {
+        this.y = y;
+    }
+    
+    public double calcularDistancia(Ponto outroPonto){
+        return Math.sqrt(Math.pow((this.x - outroPonto.x), 10)+Math.pow((this.y - outroPonto.y), 3));
+    }
+
+}
