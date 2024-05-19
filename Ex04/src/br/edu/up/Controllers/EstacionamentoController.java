d2a93e82e0c5e478a14f15d0a6d5e5066f64d25f
MarlonVolff <marlonvolffsilva@gmail.com>
2024-05-19 18:31:07 -0300

    Ex04

---
+++ Ex04/src/br/edu/up/Controllers/EstacionamentoController.java
@@ -0,0 +1,107 @@
+package br.edu.up.controllers;
+
+import java.util.regex.Pattern;
+
+import br.edu.up.models.*;
+
+public class EstacionamentoController {
+
+    private int contEntrada = 0;
+    private int carrosSegundoPeriodo;
+    
+
+    Estacionamento est = new Estacionamento();
+
+    public String registrarEntrada(String placa, String modelo, String cor) {
+        int vagaDisponivel = est.encontrarVagaDisponivel();
+        if (vagaDisponivel != -1) {
+            if (validarPlaca(placa) == true) {
+                contEntrada++;
+                est.getVagas(vagaDisponivel).ocuparVaga(placa, modelo, cor);
+                return  "OK";
+            }else{
+                return "null";
+            }
+        } else {
+            return "null";
+        }
+    }
+
+    public String registarSaida(String placa){
+        for(int i = 0; i < 10; i++){
+            if (est.verificarPlaca(placa) != null && est.verificarPlaca(placa).equals("1")) {
+                if (est.getVagas(i).getCarroPlaca() != null && est.getVagas(i).getCarroPlaca().equals(placa)) {
+                    est.getVagas(i).liberarVaga();
+                    return "OK"; // Retorna "OK" se a placa for encontrada e a vaga for liberada
+                }
+            }
+        }
+        return "OFF"; // Retorna "OFF" se a placa não for encontrada ou se a vaga não estiver ocupada por esse veículo
+    }
+    
+
+
+    public int contarVagasDisponiveis() {
+        int contador = 0;
+        for (Vaga vaga : est.getVagas()) {
+            if (!vaga.ocupada()) { 
+                contador++; 
+            }
+        }
+        return contador;
+    }
+    public int consultarVaga(String placa){
+        for(int i=0; i<10; i++){
+            if (est.getVagas(i).getCarroPlaca().equals(placa)) {
+                return est.getVagas(i).getNumero();
+            }
+            
+        }
+        return 0;
+    }
+    public Double finalizarPeriodo(){
+        Double valorFinal = contEntrada*5.00;
+        return valorFinal;
+    }
+
+    public Double finalizarPeriodoTarde(){
+        double valorFinal = (contEntrada+ carrosSegundoPeriodo) * 5.00;
+        return valorFinal;
+    }
+
+    public int carrosSegundoPeriodo(){
+        this.carrosSegundoPeriodo = 10- contarVagasDisponiveis();
+        return carrosSegundoPeriodo;
+    }
+    public double finalizarPeriodoNoite(){
+        double valorFinal = (contEntrada + carrosSegundoPeriodo) * 5.00;
+        return valorFinal;
+    }
+
+    public int getContEntrada() {
+        return contEntrada;
+    }
+    
+    
+
+    public int getCarrosSegundoPeriodo() {
+        return carrosSegundoPeriodo;
+    }
+
+    public void setContEntrada(int contEntrada) {
+        this.contEntrada = contEntrada;
+    }
+
+    public boolean validarPlaca(String placa){
+        String[] partes = placa.split("-");
+        if(partes.length != 2 || partes[0].length() != 3) {
+            return false;
+        }
+        // Verifica se a segunda parte consiste apenas em dígitos ou letras
+        if (!partes[1].matches("[0-9a-zA-Z]+")) {
+            return false;
+        }
+        return true;
+    }
+}
+ 
